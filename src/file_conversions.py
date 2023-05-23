a8e4417260dccb5d6528ffa83f316ad711def4e5
cwilson22 <csw8@buffalo.edu>
2023-05-23 11:49:05 -0400

    scotch json files

--- src/file_conversions.py
+++ src/file_conversions.py
@@ -7,13 +7,12 @@ import re
 def read_storyline():
 	for sname in os.listdir("../data/Storylines"):
 		if os.path.splitext(sname)[1] == ".txt":
-			print(sname)
 			graph = {"nodes": [], "links": []}
 			name_tsteps = {}
 			with open(f"../data/Storylines/{sname}") as f:
 				tstep = 0
 				for ln in f.readlines():
-					groups = re.split(r'[\t\n ]', ln.removesuffix('\n'))
+					groups = re.split(r'[\t ]', ln.removesuffix('\n'))
 					group_names = [re.split(',', val) for val in groups]
 					for gidx, gp in enumerate(group_names):
 						for nm in gp:
@@ -26,5 +25,24 @@ def read_storyline():
 				json.dump(graph, f)
 
 
+def read_scotch():
+	for sname in os.listdir("../data/scotch"):
+		if os.path.splitext(sname)[1] == ".src":
+			graph = {"nodes": [], "links": []}
+			link_seen = set()
+			with open(f"../data/scotch/{sname}") as f:
+				for ln in f.readlines():
+					sep = re.split(r'[\t ]', ln.removesuffix('\n'))
+					if len(sep) > 2:
+						graph["nodes"].append({"id": sep[0], "value": sep[1]})
+						for i in range(int(sep[2])):
+							lk = (sep[0], sep[2*i + 4])
+							if (lk[1], lk[0]) not in link_seen:
+								link_seen.add(lk)
+								graph["links"].append({"nodes": [lk[0], lk[1]], "value": sep[2*i + 3]})
+			with open(f"../data/scotch/{sname.replace('.src', '.json')}", 'w') as f:
+				json.dump(graph, f)
+
+
 if __name__ == '__main__':
-	read_storyline()
+	read_scotch()
