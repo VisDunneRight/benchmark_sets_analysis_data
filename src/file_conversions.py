62bb25017eab59bf9f318a5f4a7e0e1dac0be9dd
cwilson22 <csw8@buffalo.edu>
2023-06-01 12:58:32 -0400

    MID indent

--- src/file_conversions.py
+++ src/file_conversions.py
@@ -533,7 +533,7 @@ if __name__ == '__main__':
 	# read_investment()
 	# read_randdag()
 
-	direct = "../data/airlines-migration-air traffic/clean"
+	direct = "../data/MID/clean"
 	for fle in os.listdir(direct):
 		if os.path.splitext(fle)[1] == ".json":
 			with open(direct + "/" + fle) as fd1:
