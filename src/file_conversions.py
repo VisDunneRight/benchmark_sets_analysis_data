c446e55165a1c49b80e050e2003d4eee7f6193bd
cwilson22 <csw8@buffalo.edu>
2023-06-01 12:59:33 -0400

    Greenhouse gas indent

--- src/file_conversions.py
+++ src/file_conversions.py
@@ -533,7 +533,7 @@ if __name__ == '__main__':
 	# read_investment()
 	# read_randdag()
 
-	direct = "../data/MID/clean"
+	direct = "../data/world-greenhouse-gas-emissions/clean"
 	for fle in os.listdir(direct):
 		if os.path.splitext(fle)[1] == ".json":
 			with open(direct + "/" + fle) as fd1:
